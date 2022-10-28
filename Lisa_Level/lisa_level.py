c4803cdfc39b711a210f4a4a16c3acdf9ce94df2
andonivila <andonivila@gmail.com>
2022-10-28 10:31:59 +0200

    update

--- Lisa_Level/lisa_level.py
+++ Lisa_Level/lisa_level.py
@@ -1,23 +1,62 @@
+'''
+En este caso:
+Time nos permite hacer pausas durante el codigo
+OS nos permite crear carpetas 
+Urllilb.requests nos permite descargar una imagen de una URL concreta 
+Requests nos permite hacer una peticion GET a una API 
+Shutil nos permite mover un archivo a una carpeta, en este caso una foto a su carpeta
 
+'''
 import time
 import os
 import urllib.request
 import requests
 import shutil
 
+'''
+Creamos un diccionario vacio para guardar un contador de las palabras 
+
+'''
 dic={}
 i=0
 w=0
 while True:
+   
     time.sleep(2)
+    '''
+    Requests.Get crea una peticion GET (https://www.ionos.es/digitalguide/hosting/cuestiones-tecnicas/http-request/)
+    '''
     resp = requests.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+    '''
+    Llamamos al contenido de la respuesta de la API con () 
+    de todo el contenido, nos interesa el primer diccionario [0]
+    y de todo el diccionario queremos el valor de la llave ['quote']
+    '''
     personaje=resp.json()[0]['character']
     frase=resp.json()[0]['quote']
+    '''
+    URL guarda la URL donde se encuentra la imagen que quremos descargar
+    '''
     url=resp.json()[0]['image']
+    '''
+    Con la variable file podemos darle un nombre a la imagen que vamos a descargar
+    igual que el personaje, y ademas le damos la extension .png que nos permite 
+    abrirla 
+    '''
     file= (f"Esta es la foto de {personaje}.png")
 
+    '''
+    No podemos tener 2 directorios con el mismo nombre, 
+    por eso if not nos permite comprobar si existe ya una carpeta 
+    que se llame como el personaje 
+
+    Si no existe descargaremos la imagen de la URL de la API
+    Crearemos una carpeta con el nombre del personaje 
+    Y cogeremos la imagen que se llamara  como el personaje gracias a la variable file
+    y lo meteremos en su carpeta correspondiente
+
+    '''
     if not (os.path.exists(personaje)):
-       #Descargar la imagen  
         r=urllib.request.urlopen(url)
         f=open(file,'wb')
         f.write(r.read())
--- Maggie_Level/Main/maggie_level.py
+++ Maggie_Level/Main/maggie_level.py
@@ -1,16 +1,43 @@
+'''
+En este caso:
+Requests permite pedirle informacion a la API 
+Time nos permite hacer pausas durante el codigo
+Pandas nos permite introducir los datos que queramos en un CSV
+''' 
 import requests
 import time
 import pandas as pd
 
+#While -> True crea un bucle infinito
 while True:
     time.sleep(30)
+    #Requests.Get crea una peticion GET (https://www.ionos.es/digitalguide/hosting/cuestiones-tecnicas/http-request/)
     resp = requests.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+    #La API nos entrega un "paquete" (lista de diccionarios) con distintos datos, de los cuales solo nos interesan 2
+    '''
+    Llamamos al contenido de la respuesta de la API con () 
+    de todo el contenido, nos interesa el primer diccionario [0]
+    y de todo el diccionario queremos el valor de la llave ['quote']
+    ''' 
     frase=resp.json()[0]['quote']
     personaje=resp.json()[0]['character']
+    '''
+    Como vamos a clasificar las quote dependiendo del personaje que lo diga
+    creamos 2 variables para comparar
+    '''
     Lisa='Lisa Simpson'
     Homer='Homer Simpson'
-    print (frase)
-    print (personaje)
+    '''
+    El 1º criterio es que la frase la diga Lisa, si personaje es = Lisa Simpson
+    crearemos un diccionario con 2 llaves y 2 valores, las llaves seran Frase y Personaje
+    y sus valores descargados de la API 
+    '''
+    '''
+    Creamos un DataFrame con el diccionario y lo introducimos en el CSV, 
+    mode = 'a' nos permite introducir datos en el CSV sin sobrescribir los datos 
+    Con Header True podremos ver las llaves de nuestro diccionario, es decir Frase y Personaje
+    Y con Index False eliminamos un contador  
+    '''
     if personaje==Lisa:
         Lisa_Frase = {'Frase': [f'{frase}'], 'Personaje':[f'{personaje}']}
         df=pd.DataFrame(Lisa_Frase)
